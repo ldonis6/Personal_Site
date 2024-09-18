322590cdc6d17a4a045d02e6b701126c3e00dec6
ldonis6 <l.donis0212@gmail.com>
2024-09-18 15:19:16 -0400

    Testing Light mode / dark mode button switch

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-window.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll(".navbar a");
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.navbar a');
 
-  let currentSection = "";
+  let currentSection = '';
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop - 60; // Adjust offset for header height
     if (pageYOffset >= sectionTop) {
-      currentSection = section.getAttribute("id");
+      currentSection = section.getAttribute('id');
     }
   });
 
   navLinks.forEach((link) => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === `#${currentSection}`) {
-      link.classList.add("active");
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${currentSection}`) {
+      link.classList.add('active');
     }
   });
 });
 
-var typed = new Typed(".multiple-text", {
-  strings: ["Full-Stack Dev", "Web Designer", "Programmer", "Student"],
+var typed = new Typed('.multiple-text', {
+  strings: ['Full-Stack Dev', 'Web Designer', 'Programmer', 'Student'],
   typeSpeed: 50,
   backSpeed: 50,
   backDelay: 1000,
   loop: true,
+});
+
+// Light mode & Dark Mode Button
+
+document.querySelector('.mode').addEventListener('click', function () {
+  document.body.classList.toggle('lightmode');
 });
